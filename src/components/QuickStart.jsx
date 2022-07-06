8603284c163a22e4b5ed3033b29574996a2616ae
deineyeezysdigga <matze1610j@gmail.com>
2022-07-06 12:40:57 +0200

    06-07

--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 
 
 export default function QuickStart() {
-  return <div style={{ display: "flex", gap: "10px" }}></div>;
+  return <div style={{ display: "flex", gap: "10px", fontSize: 30 }}>Willkommen bei MediCareCoin<br></br>Hier folgt in Kürze ein Tutorial zur Nutzung der Plattform.</div>;
+
 }
